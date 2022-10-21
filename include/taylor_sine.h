eae5b2c136ed3e62975a2f76e8f3d76b764c90c9
mathiesschou <70001538+mathiesschou@users.noreply.github.com>
2022-10-21 15:19:59 +0200

    Add files via upload

--- include/taylor_sine.h
+++ include/taylor_sine.h
@@ -1,6 +1,8 @@
 #ifndef TAYLOR_SINE
 #define TAYLOR_SINE
+#include <stdio.h>
 
 // Add header defintion
+double taylor_sine(double x, int n) //sætter vores funktion ind i headerfilen
 
 #endif 
--- src/stack.c
+++ src/stack.c
@@ -1,25 +1,60 @@
 #include "stack.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void initialize(stack* s){
   //implement initialize here
+
+  // starter med at pointeren til NULL
+  *s = NULL; 
+
 }
 
 void push(int x, stack* s){
     //implement push here
+  
+    // allokerer hukommelse til ny node
+    node *t = (node *)malloc(sizeof(node));
+
+    // pointer mod data og så pointer mod den næste værdi
+    t->data = x;
+    t->next = *s; 
+
+    // topnoden bliver den nye node
+    *s = t; 
 }
 
 int pop(stack* s){
     // implement pop here
-  return -1;
+
+    //pre: assert hvis vores pointer ikke er i toppen
+    assert(*s != null);
+
+    //frigører node til p, og returnere værdien
+    node *p = *s;
+    *top = (*s)->next;
+
+    //tager dataen og sletter den
+    int temp = p->data;
+    free(p);
+
+  // returner dataen
+  return temp;
 }
 
 bool empty(stack* s)
 {
   //implement empty here
-  return false;
+
+  //return true, hvis top noden er NULL
+  return *s == NULL;
 }
 
 bool full(stack* s) {
     //implement full here
-  return false;
+
+    // return false, hvis top noden ikke er NULL
+  return *s != NULL;
 }
